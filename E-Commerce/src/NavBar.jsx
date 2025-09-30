1ae0f0bbde0719c6d8d6646ec7ef0b688e35e184
abhishek <abhishekpandey51628@gmail.com>
2025-09-30 13:34:02 +0530

    started

---
+++ E-Commerce/src/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+function NavBar(){
+    return (
+        <nav >
+            <h2>E-Commerce</h2>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/services">Services</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+
+    );
+}
+ export default NavBar;
